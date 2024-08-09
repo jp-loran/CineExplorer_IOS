69d21c5798ae0260ce7ce0dc6a7f66ab26cc79e8
Juan Pablo <jpalvarezloran@gmail.com>
2024-08-08 20:41:42 -0600

    added movie api key

---
+++ CineExplorer/Constants/MovieApiKey.swift
@@ -0,0 +1,12 @@
+//
+//  MovieApiKey.swift
+//  CineExplorer
+//
+//  Created by Juan Pablo Alvarez Loran on 01/08/24.
+//
+
+import Foundation
+
+struct MovieApiKey {
+    static let movieApiKey : String = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODJhYzVjOWI2NDY3OGNiZDJmNDBjZWEyNjUyYWU4OCIsInN1YiI6IjY2NDJjMjAyZTg2YmQzMTBiYTE4MmM0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O8k8e_bcLthkotp-cQ66IMYZ24fvZrTePdGUqABTEHs"
+}
